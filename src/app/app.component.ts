45c7dbf49edd671f015b6422a9715ab6a5822102
Pertsev Roman <pertsev.roman@gmail.com>
2018-04-24 16:33:37 +0300

    pixi dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss'
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private app: PIXI.Application = null;
+
+  ngOnInit() {
+    this.app = new PIXI.Application();
+  }
 }
