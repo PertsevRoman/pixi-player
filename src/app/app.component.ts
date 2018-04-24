ad430920aee0544175716a6cc5ce169e0b0675fe
Pertsev Roman <pertsev.roman@gmail.com>
2018-04-24 16:48:03 +0300

    add assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as PIXI from 'pixi.js';
+
+// FIXME использовать ng-packagr для упаковки в модули
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,11 @@ import * as PIXI from 'pixi.js';
 export class AppComponent implements OnInit {
   private app: PIXI.Application = null;
 
+  @ViewChild('canvas') canvas: ElementRef;
+
   ngOnInit() {
-    this.app = new PIXI.Application();
+    this.app = new PIXI.Application({
+      view: this.canvas.nativeElement
+    });
   }
 }
