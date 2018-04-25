760416b72f5e063c91667a417d087a3d44ec79d5
Pertsev Roman <pertsev.roman@gmail.com>
2018-04-25 13:52:02 +0300

    devices mediastream

--- src/app/devices.service.ts
+++ src/app/devices.service.ts
@@ -1,18 +1,58 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class DevicesService {
-  public audioInputs: MediaDeviceInfo[] = [];
-  public videoInputs: MediaDeviceInfo[] = [];
-  public audioOutputs: MediaDeviceInfo[] = [];
+  // устройства
+  private audioInputs: MediaDeviceInfo[] = [];
+  private videoInputs: MediaDeviceInfo[] = [];
+  private audioOutputs: MediaDeviceInfo[] = [];
 
-  stream: EventEmitter<MediaStream> = new EventEmitter();
+  // медиапоток
+  private mediaStream: MediaStream = new MediaStream();
+
+  // видео
+  public video: HTMLVideoElement = document.createElement('video');
 
   constructor() {
+    this.video.srcObject = this.mediaStream;
+
     navigator.mediaDevices.enumerateDevices().then(deviceInfos => {
       this.audioInputs  = deviceInfos.filter((info) => info.kind == "audioinput");
       this.videoInputs  = deviceInfos.filter((info) => info.kind == "videoinput");
       this.audioOutputs = deviceInfos.filter((info) => info.kind == "audiooutput");
+
+      if (!this.videoInputs.length) {
+        const fakeVideo = document.createElement('video');
+        fakeVideo.setAttribute('src', `/assets/chroma.mp4`);
+        fakeVideo.setAttribute('autoplay', `autoplay`);
+        fakeVideo.setAttribute('loop', `loop`);
+        fakeVideo.muted = true;
+
+        fakeVideo.addEventListener('loadedmetadata', () => {
+          const fakeStream: MediaStream = (fakeVideo as any).captureStream();
+          this.mediaStream.addTrack(fakeStream.getVideoTracks()[0]);
+        });
+      } else {
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            deviceId: this.videoInputs[0].deviceId
+          }
+        }).then((stream) => {
+          this.mediaStream.addTrack(stream.getVideoTracks()[0]);
+        });
+      }
     });
+
+    if (this.audioInputs.length) {
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: {
+          deviceId: this.audioInputs[0].deviceId
+        }
+      }).then((stream) => {
+        this.mediaStream.addTrack(stream.getAudioTracks()[0])
+      });
+    }
   }
 }
