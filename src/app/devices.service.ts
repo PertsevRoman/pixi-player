d50900e8cfd428c38b0a71e38d49549e43abf85f
Pertsev Roman <pertsev.roman@gmail.com>
2018-04-25 13:06:00 +0300

    chroma data; stream emitter

--- src/app/devices.service.ts
+++ src/app/devices.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 @Injectable()
 export class DevicesService {
@@ -6,10 +6,12 @@ export class DevicesService {
   public videoInputs: MediaDeviceInfo[] = [];
   public audioOutputs: MediaDeviceInfo[] = [];
 
+  stream: EventEmitter<MediaStream> = new EventEmitter();
+
   constructor() {
     navigator.mediaDevices.enumerateDevices().then(deviceInfos => {
-      this.audioInputs = deviceInfos.filter((info) => info.kind == "audioinput");
-      this.videoInputs = deviceInfos.filter((info) => info.kind == "videoinput");
+      this.audioInputs  = deviceInfos.filter((info) => info.kind == "audioinput");
+      this.videoInputs  = deviceInfos.filter((info) => info.kind == "videoinput");
       this.audioOutputs = deviceInfos.filter((info) => info.kind == "audiooutput");
     });
   }
